8c28a83706aafa6e68936a951cbe4e1babaa7c92
Max Martyanov <martyanovmax@mail.ru>
2025-06-28 22:09:32 +0300

    add ksp and dagger hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -42,6 +44,9 @@ android {
 dependencies {
     implementation(project(path=":domain"))
     implementation(project(path=":data"))
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
