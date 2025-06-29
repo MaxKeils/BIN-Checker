47abf6cbb26f203403c48319e3ccb50aa49b30aa
Max Martyanov <martyanovmax@mail.ru>
2025-06-30 00:02:57 +0300

    remove unused imports

--- app/src/main/java/max/keils/binchecker/presentation/ui/screens/history/BinHistoryScreen.kt
+++ app/src/main/java/max/keils/binchecker/presentation/ui/screens/history/BinHistoryScreen.kt
@@ -2,7 +2,6 @@ package max.keils.binchecker.presentation.ui.screens.history
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -22,7 +21,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
